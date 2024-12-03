aeed08f646792f3b7cb919bae082739a9aae2524
ngigi11287 <ngigi.11287@gmail.com>
2024-12-03 08:37:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  const square = (x) => x * x; 
+  
+
+  const add = (a, b) => a + b; 
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+
+  
